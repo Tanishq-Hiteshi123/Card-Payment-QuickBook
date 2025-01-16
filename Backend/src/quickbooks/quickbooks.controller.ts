109817a1dcb60c8f9238640b95b8bb46f975e357
Tanishq-Hiteshi123 <tanishq.yadav@hiteshi.com>
2025-01-16 12:18:13 +0530

    Card Payment Inegrated Successfully

--- Backend/src/quickbooks/quickbooks.controller.ts
+++ Backend/src/quickbooks/quickbooks.controller.ts
@@ -16,10 +16,8 @@ export class QuickbooksController {
   @Get('oauth')
   @Redirect()
   async initiateOAuth(@Session() session: any) {
-    // Generate a random state
-    const state = 'abc' + Math.floor(Math.random() * 1000); // Example: generates a 32-character random string
+    const state = 'abc' + Math.floor(Math.random() * 1000);
 
-    // Store the state in session
     console.log('State valiue is ', state);
     session.state = state;
 
@@ -28,11 +26,10 @@ export class QuickbooksController {
 
     console.log(clientId, redirectUri);
 
-    // Build the QuickBooks OAuth URL with the dynamically generated state
     const quickBooksUrl = `https://appcenter.intuit.com/connect/oauth2?client_id=${clientId}&response_type=code&scope=com.intuit.quickbooks.payment&redirect_uri=${redirectUri}&state=${state}`;
 
     return {
-      url: quickBooksUrl, // This will redirect the user to QuickBooks
+      url: quickBooksUrl,
     };
   }
 
@@ -40,54 +37,46 @@ export class QuickbooksController {
   async handleOAuthCallback(
     @Query('code') code: string,
     @Query('state') state: string,
-    @Query('realmId') realmId: string, // realmId (companyId) is passed in the callback URL
-    @Session() session: any, // Access session for storing the state
+    @Query('realmId') realmId: string,
+    @Session() session: any,
   ) {
-    // Retrieve the expected state from session
     const expectedState = session.state;
     console.log(code, state, realmId);
-    // Validate that the state matches the one you sent (to prevent CSRF attacks)
     if (state !== expectedState) {
       throw new Error('State does not match, possible CSRF attack');
     }
 
-    // Now that we've validated the state, let's retrieve the access token
-
     try {
-      // Exchange the authorization code for an access token
       const tokenData = await this.quickBooksService.getAccessToken(code);
 
-      // Store the token or return it to the user (depending on your use case)
-      return tokenData; // This can be returned as a response or saved to a session/database
+      return tokenData;
     } catch (error) {
-      // If the token exchange fails, throw an error
       throw new Error('Failed to obtain access token: ' + error.message);
     }
   }
 
-  @Get('company-info')
-  async getCompanyInfo(
-    @Query('accessToken') accessToken: string,
-    @Query('realmId') realmId: string,
-  ) {
-    try {
-      // Call service to get company info using the access token and realmId
-      const companyInfo = await this.quickBooksService.getCompanyInfo(
-        accessToken,
-        realmId,
-      );
-      return companyInfo;
-    } catch (error) {
-      throw new Error('Failed to retrieve company info: ' + error.message);
-    }
-  }
+  // @Get('company-info')
+  // async getCompanyInfo(
+  //   @Query('accessToken') accessToken: string,
+  //   @Query('realmId') realmId: string,
+  // ) {
+  //   try {
+  //     // Call service to get company info using the access token and realmId
+  //     const companyInfo = await this.quickBooksService.getCompanyInfo(
+  //       accessToken,
+  //       realmId,
+  //     );
+  //     return companyInfo;
+  //   } catch (error) {
+  //     throw new Error('Failed to retrieve company info: ' + error.message);
+  //   }
+  // }
 
   @Post('pay')
   async processPayment(
     @Body('accessToken') accessToken: string,
     @Body('amount') amount: number,
     @Body('card') card: any,
-    @Body('realmId') realmId: any,
     @Body('currency') currency: any,
     @Body('context') context: any,
   ) {
@@ -96,7 +85,6 @@ export class QuickbooksController {
         accessToken,
         amount,
         card,
-        realmId,
         currency,
         context,
       );
--- Backend/src/quickbooks/quickbooks.service.ts
+++ Backend/src/quickbooks/quickbooks.service.ts
@@ -1,24 +1,20 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
-
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class QuickbooksService {
   private clientId = 'ABdbufOtqUGwQfkcZCyZIFQAp6vfIW5ckC5PRaxQPlrXX1QE41';
   private clientSecret = 'F0PSSLoDtMGFIxuwmIdq2MAcahfZ8gyRA0VSBkGs';
   private redirectUri = 'http://localhost:3000/quickbooks/oauth/callback';
-  private sandbox = true;
+  // private sandbox = true;
 
   constructor(private httpService: HttpService) {}
-
-  //   Create the function to get the access token:
-  // In your QuickBooks service (getAccessToken method):
   async getAccessToken(authCode: string): Promise<any> {
     const url = `https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer`;
     const auth = Buffer.from(`${this.clientId}:${this.clientSecret}`).toString(
       'base64',
     );
-    // const redirectUri = process.env.REDIRECT_URL; // Ensure you use the correct redirect URI
 
     try {
       const response = await lastValueFrom(
@@ -39,42 +35,44 @@ export class QuickbooksService {
     }
   }
 
-  async getCompanyInfo(accessToken: string, realmId: string): Promise<any> {
-    const url = `https://quickbooks.api.intuit.com/v3/company/${realmId}/companyinfo/${realmId}`;
+  // async getCompanyInfo(accessToken: string, realmId: string): Promise<any> {
+  //   const url = `https://quickbooks.api.intuit.com/v3/company/${realmId}/companyinfo/${realmId}`;
 
-    try {
-      const response = await lastValueFrom(
-        this.httpService.get(url, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-          },
-        }),
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error('Failed to fetch company information: ' + error.message);
-    }
-  }
+  //   try {
+  //     const response = await lastValueFrom(
+  //       this.httpService.get(url, {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //           'Content-Type': 'application/json',
+  //         },
+  //       }),
+  //     );
+  //     return response.data;
+  //   } catch (error) {
+  //     throw new Error('Failed to fetch company information: ' + error.message);
+  //   }
+  // }
 
   //   Function to process the card payment:
   async makeCardPayment(
     accessToken: string,
     amount: number,
     card: any,
-    realmId: any,
     currency: any,
     context: any,
   ): Promise<any> {
     const url = `https://sandbox.api.intuit.com/quickbooks/v4/payments/charges`; // Use sandbox URL for testing
 
     // Construct the request body for the payment
+    const requestId = uuidv4();
+    console.log(requestId);
     const payload = {
-      amount: amount, // Payment amount
-      card: card,
+      amount,
       currency,
-      context, // Card details
-    }; // Use your realmId
+      card,
+      context,
+    };
+
     console.log(payload);
     try {
       const response = await lastValueFrom(
@@ -82,36 +80,15 @@ export class QuickbooksService {
           headers: {
             Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
+            'request-id': uuidv4().replace(/-/g, '').slice(0, 50),
           },
         }),
       );
 
       return response.data; // Return the payment response
     } catch (error) {
+      console.log(error.response.data);
       throw new Error('Failed to process payment: ' + error.message);
     }
   }
 }
-
-// {
-//   "currency": "USD",
-//   "amount": "10.55",
-//   "context": {
-//     "mobile": "false",
-//     "isEcommerce": "true"
-//   },
-//   "card": {
-//     "name": "emulate=0",
-//     "number": "4111111111111111",
-//     "expMonth": "02",
-//     "address": {
-//       "postalCode": "94086",
-//       "country": "US",
-//       "region": "CA",
-//       "streetAddress": "1130 Kifer Rd",
-//       "city": "Sunnyvale"
-//     },
-//     "expYear": "2025",
-//     "cvc": "123"
-//   }
-// }
